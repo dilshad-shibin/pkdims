89327d26d4b0022975d7cb539464750220a42b52
Dilshad Shibin <49745082+dilshad-shibin@users.noreply.github.com>
2021-04-03 15:52:00 +0530

    department footer popup testimonials added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -28,6 +28,31 @@
 			}
 		});    		
 	}
+
+	// Testimonials Carousel
+	if ($('.testimonials-carousel').length) {
+		$('.testimonials-carousel').owlCarousel({
+			loop:true,
+			animateOut: 'slideOutDown',
+    		animateIn: 'fadeUp',
+			margin:0,
+			nav:true,
+			smartSpeed: 300,
+			autoplay: true,
+			navText: [ '<span class="flaticon-back-4"></span>', '<span class="flaticon-next"></span>' ],
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:1
+				},
+				1024:{
+					items:1
+				},
+			}
+		});
+	}
 	
 
 })(window.jQuery);
